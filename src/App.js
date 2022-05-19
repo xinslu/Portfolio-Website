5b8fda87f5aeb49c692cd8395c6b05f25d7493f0
kinshukphalke <phalkekinshuk@gmail.com>
2022-05-19 15:54:03 +0400

    Working on New Version

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Body from './components/body.jsx';
+import Scroll from './components/scrolling.jsx';
+import "devicon/devicon.min.css";
+import{
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Link,
+    Redirect
+} from 'react-router-dom';
+
+//Pages
+import NotFoundPage from './pages/404'
+import NagivationBar from "./components/navbar.jsx"
+import Resume from "./pages/resume.jsx"
+import Blog from "./pages/blog.jsx";
+import BlogPage from "./pages/blogPage.jsx";
+import postBlog from "./pages/makeBlog.jsx";
+function App() {
+  return (
+    <Router>
+        <Switch>
+            <Route exact path='/' component={NagivationBar} />
+            <Route exact path="/404" component={NotFoundPage} />
+            <Route exact path="/resume" component={Resume} />
+            <Route exact path="/blog" component={Blog} />
+            <Route exact path="/blog/:id" component={BlogPage} />
+            <Route exact path="/post" component={postBlog} />
+            <Redirect to="/404" />
+        </Switch>
+    </Router>
+  );
+};
+
+export default App;
