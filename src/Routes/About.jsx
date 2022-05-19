0e63199670c3d9a08f68ec71e3b3bd30ea6dc259
kinshukphalke <phalkekinshuk@gmail.com>
2022-05-19 22:51:29 +0400

    Added About me section

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -1,40 +1,23 @@
 import "../css/about.css";
 export default function About() {
-    console.log("im here");
+    let pfp = "images/pfp.png"
     return (
       <section id="about">
-        <div className="row">
-            <div className="three columns">
-              {/*<img
+      <div class="container">
+        <div className="row align-items-center">
+            <div className="col-sm-3">
+              <img
                 className="profile-pic"
-                src={profilepic}
+                src={pfp}
                 alt="Nordic Giant Profile Pic"
-              />*/}
+              />
             </div>
-            <div className="nine columns main-col">
-              <h2>About Me</h2>
-
-              {/*<p>{bio}</p>*/}
-              <div className="row">
-                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                    {/*<span>{name}</span>*/}
-                    <br />
-                    <span>
-                      {/*{street}*/}
-                      <br />
-                      {/*{city} {state}, {zip}*/}
-                    </span>
-                    <br />
-                    {/*<span>{phone}</span>*/}
-                    <br />
-                    {/*<span>{email}</span>*/}
-                  </p>
-                </div>
-              </div>
+            <div className="col-md-9">
+              <p className="text-left display-5 text-white fw-light font-monospace">About Me</p>
+              <p className="lead text-white-50 font-monospace text-justify">Hey, I am Kinshuk Phalke, a rising sophmore at the <span className="text-white">Georgia Institute of technology, Atlanta</span>, where I major in <span className="text-white">Computer Science</span> and specialize in <span className="text-white">Systems and Architecture</span> and <span className="text-white">Intelligence</span>. I am interested in <span className="text-white">FinTech, Web3, Machine Learning Research, Programming Languages</span> and <span className="text-white">Processor Architecture</span>. When, I am not coding you can find me watching a Sci-Fi Series or trying to make a funny meme. Welcome to my personal Website, where I list my personal projects and achievements!</p>
             </div>
-          </div>
+            </div>
+        </div>
       </section>
     );
 }
