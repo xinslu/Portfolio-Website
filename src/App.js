a88a92570bd19ac12da0d2fb484779813cde506a
kinshukphalke <phalkekinshuk@gmail.com>
2022-05-19 16:52:01 +0400

    Done with Landing Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Body from './components/body.jsx';
-import Scroll from './components/scrolling.jsx';
+// import Body from './components/body.jsx';
+// import Scroll from './components/scrolling.jsx';
 import "devicon/devicon.min.css";
 import{
     BrowserRouter as Router,
@@ -12,22 +12,22 @@ import{
 } from 'react-router-dom';
 
 //Pages
-import NotFoundPage from './pages/404'
-import NagivationBar from "./components/navbar.jsx"
-import Resume from "./pages/resume.jsx"
-import Blog from "./pages/blog.jsx";
-import BlogPage from "./pages/blogPage.jsx";
-import postBlog from "./pages/makeBlog.jsx";
+import Main from './Routes/Main.jsx'
+// import NagivationBar from "./components/navbar.jsx"
+// import Resume from "./pages/resume.jsx"
+// import Blog from "./pages/blog.jsx";
+// import BlogPage from "./pages/blogPage.jsx";
+// import postBlog from "./pages/makeBlog.jsx";
 function App() {
   return (
     <Router>
         <Switch>
-            <Route exact path='/' component={NagivationBar} />
-            <Route exact path="/404" component={NotFoundPage} />
+            <Route exact path='/' component={Main} />
+{/*            <Route exact path="/404" component={NotFoundPage} />
             <Route exact path="/resume" component={Resume} />
             <Route exact path="/blog" component={Blog} />
             <Route exact path="/blog/:id" component={BlogPage} />
-            <Route exact path="/post" component={postBlog} />
+            <Route exact path="/post" component={postBlog} />*/}
             <Redirect to="/404" />
         </Switch>
     </Router>
---
+++ src/Routes/Main.jsx
@@ -0,0 +1,64 @@
+import "../css/main.css";
+import{
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Link,
+    Redirect
+} from 'react-router-dom';
+// import "bootstrap/dist/css/bootstrap.min.css";
+import ParticlesBg from "particles-bg";
+export default function Main() {
+  return (
+    <div>
+      <div id="bg">
+        <ParticlesBg type="circle" bg={true} num={17}/>
+      </div>
+      <div>
+      </div>
+
+      <div className="topnav">
+        <a className="active fw-light font-monospace" href="/">
+          HOME
+        </a>
+        <a className="active fw-light font-monospace" href="/resume">
+          RESUME
+        </a>
+        <a className="active fw-light font-monospace" href="/blog">
+          BLOG
+        </a>
+      </div>
+      <center>
+        <p className="position-relative display-1 text-white font-weight-light font-monospace title mt-5">
+          Kinshuk Phalke
+        </p>
+      </center>
+      <div>
+        <center>
+          <p className="position-relative text-justify text-white fw-light font-monospace mb-4 mt-3 abouttext">
+            A second year, Computer Science major at the Georgia Insititute of Technology, Atlanta.
+          </p>
+          <hr className="text-white-50 mb-4" width="45%" />
+        </center>
+      </div>
+      <center className="mt-2">
+        <a href="https://www.linkedin.com/in/kinshukphalke/" target="_blank">
+          <button
+            id="startButton"
+            className="me-5 btn btn-info fw-light font-monospace"
+          >
+            <i class="devicon-linkedin-plain"></i> LinkedIn
+          </button>
+        </a>
+        <a href="https://github.com/xinslu" target="_blank">
+          <button
+            id="tutorialButton"
+            className="ms-2 btn btn-secondary fw-light font-monospace"
+          >
+            <i class="devicon-github-original"></i> GitHub
+          </button>
+        </a>
+      </center>
+      </div>
+  )
+}
