bf2adf49447e89f87f3eac0daa7cf48118ac2cd9
kinshukphalke <phalkekinshuk@gmail.com>
2022-05-20 10:21:47 +0400

    Added a Footer

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -1,23 +1,43 @@
 import "../css/about.css";
 export default function About() {
-    let pfp = "images/pfp.png"
-    return (
-      <section id="about">
+  let pfp = "images/pfp.png";
+  return (
+    <section id="about">
       <div class="container">
         <div className="row align-items-center">
-            <div className="col-sm-3">
-              <img
-                className="profile-pic"
-                src={pfp}
-                alt="Nordic Giant Profile Pic"
-              />
-            </div>
-            <div className="col-md-9">
-              <p className="text-left display-5 text-white fw-light font-monospace">About Me</p>
-              <p className="lead text-white-50 font-monospace text-justify">Hey, I am Kinshuk Phalke, a rising sophmore at the <span className="text-white">Georgia Institute of technology, Atlanta</span>, where I major in <span className="text-white">Computer Science</span> and specialize in <span className="text-white">Systems and Architecture</span> and <span className="text-white">Intelligence</span>. I am interested in <span className="text-white">FinTech, Web3, Machine Learning Research, Programming Languages</span> and <span className="text-white">Processor Architecture</span>. When, I am not coding you can find me watching a Sci-Fi Series or trying to make a funny meme. Welcome to my personal Website, where I list my personal projects and achievements!</p>
-            </div>
-            </div>
+          <div className="col-sm-3">
+            <img
+              className="profile-pic"
+              src={pfp}
+              alt="Nordic Giant Profile Pic"
+            />
+          </div>
+          <div className="col-md-9">
+            <p className="text-left display-5 text-white fw-light font-monospace">
+              About Me
+            </p>
+            <p className="lead text-white-50 font-monospace text-justify">
+              Hey, I am Kinshuk Phalke, a rising sophmore at the{" "}
+              <span className="text-white">
+                Georgia Institute of technology, Atlanta
+              </span>
+              , where I major in{" "}
+              <span className="text-white">Computer Science</span> and
+              specialize in{" "}
+              <span className="text-white">Systems and Architecture</span> and{" "}
+              <span className="text-white">Intelligence</span>. I am interested
+              in{" "}
+              <span className="text-white">
+                FinTech, Web3, Machine Learning Research, Programming Languages
+              </span>{" "}
+              and <span className="text-white">Processor Architecture</span>.
+              When, I am not coding you can find me watching a Sci-Fi Series or
+              trying to make a funny meme. Welcome to my personal Website, where
+              I list my personal projects and achievements!
+            </p>
+          </div>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 }
---
+++ src/Routes/Footer.jsx
@@ -0,0 +1,21 @@
+import "../css/footer.css";
+export default function Footer() {
+    return (
+        <footer>
+            <div className="row">
+                <div className="twelve columns">
+                  <ul className="social-links">Bruh</ul>
+                  <ul className="copyright">
+                    <li>&copy; Copyright 2022 Kinshuk Phalke</li>
+                  </ul>
+                </div>
+
+              <div id="go-top">
+                <a className="smoothscroll" title="Back to Top" href="/">
+                  <i className="icon-up-open"></i>
+                </a>
+              </div>
+            </div>
+      </footer>
+    );
+}
