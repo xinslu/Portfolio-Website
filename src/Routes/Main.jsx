b17f47b66b870c0651e6cdc66a9baa44c3eed985
kinshukphalke <phalkekinshuk@gmail.com>
2022-05-20 19:02:28 +0400

    Scripting aahhhh

--- src/Routes/Main.jsx
+++ src/Routes/Main.jsx
@@ -23,13 +23,7 @@ export default function Main() {
   };
   const [offset, setOffset] = useState(0);
 
-  useEffect(() => {
-    const onScroll = () => setOffset(window.pageYOffset);
-    // clean up code
-    window.removeEventListener("scroll", onScroll);
-    window.addEventListener("scroll", onScroll, { passive: true });
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
+
   return (
     <div style={{ height: "100vh" }}>
       <header id="home">
