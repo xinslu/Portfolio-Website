c831d3cb824e83a58c8f64d84ede88355af9fea5
kinshukphalke <phalkekinshuk@gmail.com>
2022-05-20 22:16:22 +0400

    Sorting Package Issues

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import 'font-awesome/css/font-awesome.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+// import '@fortawesome/react-fontawesome';
 
 ReactDOM.render(
   <React.StrictMode>
