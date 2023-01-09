ce1dccc7f80e18878305ce2ec5434986e3f0c506
Kinshuk Phalke <phalkekinshuk@gmail.com>
2023-01-09 17:45:02 -0500

    Added some things

---
+++ src/contents/experiences.js
@@ -0,0 +1,16 @@
+let experiences = [
+    {
+        title: "NCR Corporation",
+        job: "Software Developer",
+        duration: "May 2022 - August 2022",
+        description: `For 12 weeks, I worked at the NCR Corporation Headquarters in Altanta, GA. I worked on the <span class="highlight">Retail Innovation - Edge Team</span>. <br> <br> With a team of 5 other interns, I worked on the <span class="highlight"> GoKart Selling Service</span>, which is a API written in the <span class="highlight">Fiber Web Server</span> in Golang. This service allows for the support of <span class="highlight">creation, storage, and management of a cart object </span> achieved with a unique cart identifier. The cart supports the inclusion of items retrieved through requests to the <span class="highlight"> NCR BSP Catalog API</span>, and store locations through to the <span class="highlight">BSP Site API</span>. The cart also supports a selling engine and its pertinent calculations like calculating gross-totals, totals with promotions applied, exemptions applied, and taxes applied, as well as the calculation and handling of returns. The app is meant to work with exsisting <span class="highlight"> retail e-commerce apps </span> and <span class="highlight">Point of Sale systems</span>. This was made to the replace the existing .NET API as this was rather bulky and slow, we managed to cut down the size of the binary from 100MB to just <span class="highlight"> 5MB </span> at comparable performance. <br> <br> Along with the api, we worked on a sample e-commerce website written in <span class="highlight">Next.js </span>on incorporating API into the frontend. To help with development, I also made a <span class="highlight">bash script </span> with 12 different presets to help in development, building, running and deployment to Google Cloud Platform.`
+    },
+    {
+        title: "Notitia",
+        job: "Software Developer",
+        duration: "October 2021 - Present",
+        description: `I am working in a start up at Georgia Tech in building a MVP. I worked on developing a <span class="highlight">Full-Stack Website</span> and working on fine tuning an NLP model for more accurate results. I worked on a machine learning pipeline that uses <span class="highlight">UMAP</span> to map the relevant word embeddings of pieces of text onto a 2D graph, <span class="highlight">FAISS</span> to effieciently cluster these texts and an <span class="highlight">LDA model</span> on these clusters to find accurate topics for these. In the frontend, I worked on optimizing WordCloud of test using <span class="highlight">ReactFlow</span>.`,
+    }
+];
+export default experiences;
+
---
+++ src/contents/skills.js
@@ -0,0 +1,248 @@
+let skills = [
+    {
+        title: "Programming Languages",
+        rows: [
+                {
+                    field: "python-plain",
+                    title: "Python",
+                },
+                {
+                    field: "javascript-plain",
+                    title: "JavaScript",
+                },
+                {
+                    field: "rust-plain",
+                    title: "Rust",
+                },
+                {
+                    field: "go-plain",
+                    title: "Golang",
+                },
+                {
+                    field: "java-plain",
+                    title: "Java",
+                },
+                {
+                    field: "c-plain",
+                    title: "C",
+                },
+                {
+                    field: "csharp-plain",
+                    title: "C#",
+                },
+                {
+                    field: "latex-plain",
+                    title: "LaTeX",
+                },
+                {
+                    field: "solidity-plain",
+                    title: "Solidity",
+                },
+                {
+                    field: "bash-plain",
+                    title: "Bash",
+                },
+                {
+                    field: "cplusplus-plain",
+                    title: "C++",
+                },
+                {
+                    field: "typescript-plain",
+                    title: "Typescript",
+                },
+                {
+                    field: "haskell-plain",
+                    title: "Haskell",
+                },
+                {
+                    field: "ocaml-plain",
+                    title: "OCaml",
+                },
+        ],
+    },
+    {
+        title: "Frameworks",
+        rows: [
+                {
+                    field: "react-plain",
+                    title: "ReactJS",
+                },
+                {
+                    field: "express-original",
+                    title: "Express.js",
+                },
+                {
+                    field: "react-plain",
+                    title: "React Native",
+                },
+                {
+                    field: "nodejs-plain",
+                    title: "Node.js",
+                },
+                {
+                    field: "nextjs-plain",
+                    title: "NextJS",
+                },
+                {
+                    field: "firebase-plain",
+                    title: "Firebase",
+                },
+                {
+                    field: "flask-plain",
+                    title: "Flask",
+                },
+                {
+                    field: "androidstudio-plain",
+                    title: "Android Studio",
+                },
+                {
+                    field: "ionic-original",
+                    title: "Ionic Framework",
+                },
+                {
+                    field: "angularjs-plain",
+                    title: "AngularJS",
+                },
+                {
+                    field: "flutter-plain",
+                    title: "Flutter",
+                },
+                {
+                    field: "bootstrap-plain",
+                    title: "Bootstrap",
+                },
+                {
+                    field: "bulma-plain",
+                    title: "Bulma",
+                },
+                {
+                    field: "gatsby-plain",
+                    title: "Gatsby",
+                },
+                {
+                    field: "handlebars-plain",
+                    title: "Handlebar.js",
+                },
+                {
+                    field: "django-plain",
+                    title: "django",
+                },
+                {
+                    field: "socketio-plain",
+                    title: "Socket.IO",
+                },
+        ],
+    },
+    {
+        title: "Libraries",
+        rows: [
+                {
+                    field: "numpy-plain",
+                    title: "Numpy",
+                },
+                {
+                    field: "pandas-plain",
+                    title: "Pandas",
+                },
+                {
+                    field: "tensorflow-original",
+                    title: "Tensorflow",
+                },
+                {
+                    field: "jupyter-plain",
+                    title: "Jupyter Notebook",
+                },
+                {
+                    field: "pytorch-original",
+                    title: "PyTorch",
+                },
+                {
+                    field: "opencv-plain",
+                    title: "OpenCV",
+                },
+        ],
+    },
+    {
+        title: "Database",
+        rows: [
+                {
+                    field: "mysql-plain",
+                    title: "SQl",
+                },
+                {
+                    field: "mongodb-plain",
+                    title: "MongoDB",
+                },
+                {
+                    field: "redis-plain",
+                    title: "Redis",
+                },
+        ],
+    },
+    {
+        title: "Technologies",
+        rows: [
+                {
+                    field: "git-plain",
+                    title: "Git",
+                },
+                {
+                    field: "github-plain",
+                    title: "GitHub",
+                },
+                {
+                    field: "docker-plain",
+                    title: "Docker",
+                },
+                {
+                    field: "unity-original",
+                    title: "Unity",
+                },
+                {
+                    field: "vim-plain",
+                    title: "Vim",
+                },
+                {
+                    field: "linux-plain",
+                    title: "Linux",
+                },
+                {
+                    field: "googlecloud-plain",
+                    title: "GCP",
+                },
+                {
+                    field: "amazonwebservices-plain",
+                    title: "AWS",
+                },
+                {
+                    field: "figma-plain",
+                    title: "Figma",
+                },
+                {
+                    field: "xd-plain",
+                    title: "AdobeXD",
+                },
+                {
+                    field: "matlab-plain",
+                    title: "Matlab",
+                },
+                {
+                    field: "nginx-plain",
+                    title: "nginx",
+                },
+                {
+                    field: "kubernetes-plain",
+                    title: "Kubernetes",
+                },
+                {
+                    field: "raspberrypi-line",
+                    title: "Raspberry Pi",
+                },
+                {
+                    field: "heroku-plain",
+                    title: "Heroku",
+                },
+        ],
+    },
+];
+export default skills;
