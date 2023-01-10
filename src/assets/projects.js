0efc1bd6c68ccd4ae6e1fdaa54e8aa65d274d43f
Kinshuk Phalke <phalkekinshuk@gmail.com>
2023-01-09 19:22:38 -0500

    Added some things

---
+++ src/assets/projects.js
@@ -0,0 +1,58 @@
+const projects = [
+    {
+        title: "Speech2Code",
+        link: "https://github.com/saisree27/speechtocode",
+        tech: "Python, ReactJS, Express.js",
+        description: `Speech2Code is a <span class="highlight"> turing-complete website</span> that helps translate a spoken english like <span class="highlight"> meta language </span> to Java, JavaScript and Python, using a <span class="highlight"> language agnostic class-based abstraction system </span> that enables easy addtion of language support. The speech transcription is done using the <span class="highlight"> AssemblyAI API </span>. I worked on making the Programming Language Middle-End and worked on making CSS changes on the front-end website.`,
+    },
+    {
+        title: "DetectCNN",
+        link: "https://github.com/xinslu/DetectCNN",
+        tech: "OCaml, Owl, Eigen",
+        description: `DetectCNN is my implementation of the <span class="highlight">Mask Region Based Convolutional Neural Network</span> Paper, written in OCaml using Owl and Eigen. Worked on sorting <span class="highlight">LLVM</span> issues in Owl on <span class="highlight">Darwin M1</span> by contributing to the Github issue. Read and summarized findings of the <span class="highlight">  R-CNN, Fast R-CNN, Faster R-CNN Papers </span>. Studied <span class="highlight"> Instance and Semantic Segmentation approaches, Region Proposal Techniques </span> and <span class="highlight"> Feature Extraction Methods. </span>`,
+    },
+    {
+        title: "Enhance",
+        link: "https://github.com/xinslu/Enhance",
+        tech: "Python, Tensorflow, Keras",
+        description: `Enhance is my implementation of the <span class="highlight">Super Resolution Generative Adverserial Network</span> Paper using tensorflow, which uses a ResNet with a <span class="highlight">Perceptual Loss function</span> based on discriminator probability and the euclidean distance between the feature representations of a <span class="highlight">reconstructed image</span> and <span class="highlight">reference image</span>, to achieve a high level of super resolution. Ran and trained the model on the DIV2K dataset achieving perceptible improvements in image quality on the <span class="highlight">DIV2K dataset with downscale factor 4 </span>`,
+    },
+    {
+        title: "DeusCL",
+        link: "https://github.com/xinslu/deusCL",
+        tech: "Rust, Yew Framework",
+        description: `DeusCL is a memory-safe <span class="highlight">Common LISP</span> implementation with a REPL built in <span class="highlight">Rust</span> . Built a handwritten <span class="highlight">recursive descent parser </span> and a visitor pattern dependent evaluator. Added a basic <span class="highlight">type inference</span>, <span class="highlight">type checking </span> and <span class="highlight">error handling system</span>. Added <span class="highlight">dynamic evaluation of functional environment </span> and parameters. Wrote a simple <span class="highlight">garbage collector </span> and environment cleaner. Built a frontend in <span class="highlight">Yew Framework </span>in Rust to enable users to use a web based simulation of REPL.`,
+    },
+    {
+        title: "PaperPlanes",
+        link: "https://github.com/AllisanLu/PaperPlanes",
+        tech: "Unity, C#",
+        description: `Working on Paper Planes, a <span class="highlight">2D sidescroller platformer</span> created with <span class="highlight">Unity</span> as a part of the VGDev Club on campus. I worked on a <span class="highlight">relaunch mechanism</span> using custom aero dynamics, a <span class="highlight">particle system</span> for paper plane trail, and one for <span class="highlight">collisions with objects</span>. I also worked on making a <span class="highlight">Quest API</span> to show the current quests going on.`,
+    },
+    {
+        title: "hack-a-lang",
+        link: "https://github.com/xinslu/hack-a-lang",
+        tech: "Python",
+        description: `As a part of a dependently-typed (Programming Language Club) on campus, I worked on a <span class="highlight">python implementation</span> of the common teaching language Lox. I worked on building and degubbing a <span class="highlight">Recursive Descent Parser, Scanner</span> and a made a script to generate a <span class="highlight">python class system</span> for different atoms and a <span class="highlight">visitor pattern</span> for the interpreter class. I also worked on the <span class="highlight">AST</span> and a <span class="highlight">language environment</span> to store function and variable declration. I lectured at the event about <span class="highlight">AST evaluation, dynamic environment creation</span> for functional and recrusive calls and <span class="highlight">implementating the vistitor patters </span> for interpreters.`,
+    },
+    {
+        title: "Publixmon",
+        link: "https://github.com/saisree27/publixmon",
+        tech: "ReactJS, Express.js, Flask, Firebase, Solidity, Ganache, IPFS, Web3.js, NCR API",
+        description: `Publixmon is a <span class="highlight">full-stack mobile app</span> integrating modern day NFT and Style Tranfer technologies to create a unique interactive retail experience. With every checkout, the app has a chance to give the user a NFT using a <span class="highlight">style tranfer machine learning model</span> based on two attributes a famous painting by a <span class="highlight">renaissance painting</span> and an item in your <span class="highlight">purchase history</span>. Users walk into a store and scan a QR code to see other active users who are in the store and they can <span class="highlight">socialize</span>, see each other's collections of virtual toys, and <span class="highlight">trade</span> them to grow their collection. A user's collection is auto-scored based on size and <span class="highlight">variety of collectibles</span>, resulting in <span class="highlight">higher engagement</span> for the business. I worked on making the backend blockchain that generates the NFTs that uses the style tranfer images using <span class="highlight">Solidity</span> and a <span class="highlight">Ganache Blockchain</span> to test it on. I worked on creating the backend for the project that uses the <span class="highlight">NCR API</span> to serve purchase history and <span class="highlight">CDN Network</span> to store the style transfered images.`,
+    },
+    {
+        title: "Mental Health Chat",
+        link: "https://github.com/xinslu/mental-health-chat",
+        tech: "ReactJS, Express.js, MongoDB, Firebase, Socket.IO",
+        description: `A timed chat for people to share and talk about issues they're having in relation with their mental health and help them overcome it. A message in the <span class="highlight">Redis database</span> to expire in about <span class="highlight">6 hours</span> in order to encourage people to come out and discourage users from harrasing one another. Created a <span class="highlight">Dockerized App</span> with a Redis Instance during development. Set-up a Log-In page using <span class="highlight">Firebase</span> that is capable of using <span class="highlight">Google OAuth</span>, the users can then set their username, add a profile picture and set the theme (light/dark) for the app, before getting started chatting on the server.`,
+    },
+    {
+        title: "Twitake",
+        link: "https://twitake.netlify.app/",
+        tech: "ReactJS, Express.js, MongoDB, Firebase, Twitter API, Gensim, NLTK, Node.js",
+        description: `A <span class="highlight">full-stack fake disaster tweet analysis tool</span> that uses <span class="highlight">TwitterAPI</span> to extract the text from the tweet link that the user pastes in input box and data processing tools like <span class="highlight">lemmatization, tokenization, N-Grams, Stop Words </span>, removal of punctuation, and a <span class="highlight">TD-IDF based Logistic Regression NLP model</span> with a 81% accuracy. Used MongoDB, to store user hashed user credentials, created a token authentication system and used localstorage to automatically sign-in and verify the user.`,
+    },
+];
+export default projects;
+
---
+++ src/contents/first-post.md
@@ -0,0 +1,113 @@
+---
+author: Kinshuk Phalke
+datetime: 2021-10-08T15:22:00Z
+title: A Year In review
+slug: year-in-review
+draft: false
+description:
+  Reviewing 2021 from my perspective.
+---
+## table of contents
+## introduction
+First things first, Welcome to my Blog. It’s finally been done. We moved on
+from the placeholder text to this first post. Don’t worry, I am not lazy to
+write it’s just that I haven’t been able to take out much time to make some of
+the necessary backend and database to achieve this. But I haven’t yet thought
+of a name so guess I would have to call it just “Blog” for now. And I hate
+introductions, so I promise I’m doing this only once. With that out of the way,
+lets actually begin, shall we? 
+
+## "the incident"
+The other day I get a notification on my phone when sleeping and things like
+these generally don’t wake me up, but this was different like it had like the
+annoying Silk Ringtone on it. “College lol” it said, on the corner was written
+“52 Weeks Ago”. 52 Weeks Ago, I had, for no reason at all, decided to prank my
+future self. Do you know what waking up to Silk is like? It’s like that
+annoying kid you meet that just won’t stop talking, you want him to stop but
+you don’t want to hurt his feelings. Silk is like that its smooth, groovy, and,
+quite honestly, peaceful; but it makes you want to hurl a brick at your phone. 
+
+Okay maybe we got a little too sidelined there. So yeah, I woke up shutdown the
+alarm and began thinking about the past year. And I’ll be honest I didn’t
+really want to mention COVID here because you probably would have read multiple
+different articles and takes on it. But you can’t avoid it in certain
+conditions, take it from a high school senior who didn’t write final exams for
+example. I’ll try and avoid it as much as possible but as a student my whole
+life was kinda defined by it at one point. Hangouts were basically extended
+brawlhalla sessions, break probably meant going to the kitchen to get some food
+and sleep just meant getting called on by the teacher multiple times and making
+lame excuses that your mic wasn’t working, and boredom meant pranking your
+future self. 
+
+## what i learned
+So anyways there is a lot to unpack in the last year, more importantly what it
+meant for my life. A big part of my senior year was consumed by college
+applications, thinking about my major and potentially what I want to do in
+life. I know it is beyond cringe, but my life’s worth living for when I create
+something new, and I’d like to think at some point in my life I would be part
+or I daresay lead the creation of products that could one day impact the world.
+To me there is nothing more fun than creating something new, whether it is the
+new stuff that you get to learn along with it or just getting stuck on a
+problem for days at end. That’s what truly drives me. I feel like Computer
+Science, my major, is just the means that would help me get there. Being at the
+core of almost everything today, I expect to be somewhat helpful in creating
+new inventive technologies. It’s very interesting to think how many
+revolutionary technologies could be achieved by the end of our time on earth.
+Fusion Energy, Photonic Quantum Computing, Deep Space Exploration are just some
+that pop off the top of my head. In a way, I think I found myself more through
+the college application process. I know it’s beyond tiring to write an essay,
+but I found myself thinking harder about who I am and more importantly who I
+want to be as a person. In a way, college applications made me, sort of,
+connect with myself better and I found myself more confident at the end of it.
+I would admit though, the season did turn out to be bit of a disappointment for
+me. Now before you get mad at me, I did get into Georgia Tech and I’ll admit
+the vibe was a match ever since I found out about it, but hey what’s life
+without a little ambition, right? Every once a while you’ll fail and you’ll
+find one of either two things: your limit to work or how far you have to work
+to get to where you want, its up to you to choose. In case you didn’t notice
+that was my way of being Master Oogway.  Anyways, this was supposed to be a fun
+blog not a boring life lesson one.
+
+## going to college
+Now, we really get to the fun part, me coming here to Tech.  I should say
+though travelling halfway across the world in a stuffy airplane is not my kind
+of idea to get to the USA but it was the only way. I took a 14h flight from Abu
+Dhabi to Chicago and a 2h one to get here from Chicago. But my parents couldn’t
+fly here because they didn’t have a Visa so this trip was all me. We did have a
+bit of a hiccup along the way. We were kind of late for the connecting flight,
+but that was also my first introduction to American airlines; you literally
+cannot tell how many things can delay your flight but, in my case, it was a
+broken carry-on luggage compartment door. You know those things above your head
+that you close, it was off its hinges. We ended up getting delayed by 45
+minutes after literally running to the flight during the final call.  But yeah,
+we got here safe and sound, stayed in one of the best hotels I’ve stayed in so
+far. I don’t know whether it was the best hotel purely because it was a good
+one or because I had what probably would be one of my best sleeps in my entire
+life (Jetlag does hit hard). There definitely is a lot of unpack after this,
+particularly with my experience here, but hey this is just one year that we’re
+talking about right now, right?
+
+
+## what i learned (tech edition)
+Finally, I don’t think this post would be over without me talking about me
+expanding my technical knowledge this year. But as every programmer has his
+beginning mine was Mr. Robot. It’s a pretty cool series about a vigilante
+hacker who seeks to expose the darkest secrets or people and how he crashes the
+world economy, erasing the world debt and redistributing the world’s money, by
+hacking into a multinational conglomerate and shutting them out by a random
+hash key. This series really showed me what computers could do and not just in
+a harmful way but also how much it can accomplish. So, I picked up my keyboard
+and started typing as I learnt Python and Machine Learning. I learnt how to do
+Supervised and Un-Supervised learning, I learnt how to make neural networks and
+how to work with NLP models. I also learnt JavaScript because of the YouTube
+algorithm and ended up getting into Web and App Development. I learnt ReactJS
+and I built this website! But I found myself unable to complete some of the
+deeper aspects of this Website, so I set out to learn I Node.js, Express.js and
+MongoDB and along the way I coded 5 more full-stack apps which used multiple
+different technologies. I used Twitter API and a Random Forest NLP model to
+analyze tweet text in one website, whereas in the other I used Redis, Docker
+and Socket.IO to build a functioning Chat app that deletes chats within 6
+hours. Currently, I’m learning Reinforcement Learning and Data Structures an
+Algorithm. 
+
+I’m sorry I kind of rushed through the end but this is a pretty big blog post already. So I guess this is the end of it.
