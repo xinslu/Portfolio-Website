55f7b068ebac579656c51daf343211957272d023
Kinshuk Phalke <phalkekinshuk@gmail.com>
2023-01-17 16:45:08 -0500

    Added some things

--- src/assets/socialIcons.ts
+++ src/assets/socialIcons.ts
@@ -1,6 +1,4 @@
-import type { SocialIcons } from "src/types";
-
-const socialIcons: SocialIcons = {
+let socialIcons = {
   Github: `<svg
     xmlns="http://www.w3.org/2000/svg"
     class="icon-tabler"
